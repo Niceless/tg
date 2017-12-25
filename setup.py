0f056fab9b1e0c883dd26846fa0e4e1e4c30da3b
Tan Yang <598862907@qq.com>
2017-12-25 18:54:57 +0800

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='tg',
       version='1.0',
 	  description='OpenShift App',
 	  author='Tan Y',
-	  author_email='pat1encelos@tongji.edu.cn'
-	  url='http://www.python.org/sigs/distutils-sig/',
+	  author_email='pat1encelos@tongji.edu.cn',
+      url='http://www.python.org/sigs/distutils-sig/',
 	  install_requires=['Flask>=0.10.1'],
 	  )
