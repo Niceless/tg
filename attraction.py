dcd6f4c706247191eef987de68407b50501d76f5
Tan Yang <598862907@qq.com>
2017-12-25 21:31:45 +0800

    update

--- attraction.py
+++ attraction.py
@@ -3,31 +3,28 @@ import requests
 from bs4 import BeautifulSoup
 
 class SpiderMain(object):
- def Crawl(self):
-  class attraction:
-     name = ""
-     point=""
-  response = requests.get("http://www.cncn.com/paihang/")
-  soup = BeautifulSoup(response.text, "html.parser")
-  attractions = []
-  for day in soup.findAll(class_='child-noborder'):
-     i = day.findAll("li")
-     n = 0
-     b = attraction()
-     for div in i:
-         if n==0:
-             b.name = div.a.string
-             b.point=div.em.string
-         elif n == 1:
-             b.name = div.a.string
-             b.point = div.em.string
-         elif n == 2:
-             b.name = div.a.string
-             b.point = div.em.string
-         elif n == 3:
-             n = 0
-             attractions.append(b)
-             break
-         n += 1
-     return attractions
+ def Crawl(self,url):
+     hot_url=url
+     if url is None:
+         return None
+     response = requests.get(hot_url)
+     soup = BeautifulSoup(response.text, "html.parser")
+     attraction = []
+     for day in soup.findAll(class_='child-noborder'):
+         i = day.findAll("li")
+         n = 0
+         for div in i:
+             attractions = {}
+             if n == 0:
+                 attractions['name']=div.a.string
+                 attractions['point']=div.em.string
+             elif n == 1:
+                 attractions['name'] = div.a.string
+                 attractions['point'] = div.em.string
+             elif n == 2:
+                 attractions['name'] = div.a.string
+                 attractions['point'] = div.em.string
+             elif n==3:
+                 attraction.append(attractions)
+             return attraction
 
--- helloflask.py
+++ helloflask.py
@@ -3,13 +3,14 @@ from flask import jsonify
 import attraction
 app=Flask(__name__)
 @app.route('/')
-def test_flask():
-    return 'test_flask'
+def hello_flask():
+    return 'hello_flask'
 
 @app.route('/')
 def spider_hot():
+    root_url='http://www.cncn.com/paihang/'
     spider=attraction.SpiderMain()
-    attractions=spider.Crawl()
+    attractions=spider.Crawl(root_url)
     return jsonify(attractions)
 
 if __name__=='__main__':
